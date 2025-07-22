aa6eac516ba2c6034330f94f969496bbefee42a4
ReyhanHerdi <reyhanherdiyanto180603@students.unnes.ac.id>
2025-07-22 16:31:31 +0700

    add dropdown

--- flutter_app/lib/screen/input_screen.dart
+++ flutter_app/lib/screen/input_screen.dart
@@ -10,14 +10,15 @@ class InputScreen extends StatefulWidget {
 class _InputScreenState extends State<InputScreen> {
   final _namaController = TextEditingController();
   final _deskripsiController = TextEditingController();
-  final _kategoriController = TextEditingController();
   final _gambarController = TextEditingController();
   final _hargaController = TextEditingController();
+  
+  final List<String> _kategoriList = ['Makanan', 'Elektronik', 'Buku dan ATK', 'Fashion', 'Lainnya'];
+  String _selectedKategori = 'Lainnya';
 
   void _simpanProduk() async {
     if (_namaController.text.isEmpty ||
         _deskripsiController.text.isEmpty ||
-        _kategoriController.text.isEmpty ||
         _gambarController.text.isEmpty ||
         _hargaController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -28,7 +29,7 @@ class _InputScreenState extends State<InputScreen> {
     final newProduct = Product(
       nama: _namaController.text,
       deskripsi: _deskripsiController.text,
-      kategori: _kategoriController.text,
+      kategori: _selectedKategori,
       gambar: _gambarController.text,
       harga: _hargaController.text,
     );
@@ -58,11 +59,25 @@ class _InputScreenState extends State<InputScreen> {
                 labelText: 'Deskripsi',
               ),
             ),
-            TextField(
-              controller: _kategoriController,
+            SizedBox(height: 16),
+            DropdownButtonFormField<String>(
+              value: _selectedKategori,
               decoration: const InputDecoration(
                 labelText: 'Kategori',
+                border: OutlineInputBorder(),
               ),
+              items: _kategoriList.map((kategori) {
+                return DropdownMenuItem<String>(
+                  value: kategori,
+                  child: Text(kategori),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedKategori = value!;
+                });
+              },
+              validator: (value) => value == 'empty' ? 'Pilih kategori' : null,
             ),
             TextField(
               controller: _gambarController,
